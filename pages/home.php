e6ad0ce5918891007a4f5c5528bf719e44e9e993
enoch2-hub <enoch.perera.coding2@gmail.com>
2024-05-31 23:00:09 +0530

    navbar responsive-1

--- pages/home.php
+++ pages/home.php
@@ -7,9 +7,45 @@ session_start();
 <head>
     <title>Tech Store</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <script>        
+        function toggleDrawer() {
+            document.querySelector('.navbar').classList.toggle('open');
+        }
+    </script>
 </head>
 <body>
+
+
+
     <div class="navbar">
+        <a href="index.php?page=home" class="logo" style="text-decoration:none">
+            <h1 class='font3'>T S<div class="font2">Tech Store</div></h1>
+        </a>
+        <button class="drawer-toggle" onclick="toggleDrawer()">&#9776;</button>
+        <div class="navlinks">
+            <a href="index.php?page=home">Home</a>
+            <a href="index.php?page=products">Products</a>
+            <a href="index.php?page=cart">Cart</a>
+            <?php if (isset($_SESSION['user'])): ?>
+                <a href="index.php?page=profile">
+                    <img src="img/<?php echo $_SESSION['user']['profile_pic']; ?>" alt="Profile Picture" class="profile-pic">
+                    <?php echo $_SESSION['user']['username']; ?>
+                </a>
+                <a href="index.php?page=logout">Logout</a>
+            <?php else: ?>
+                <a href="index.php?page=login">Login</a>
+                <a href="index.php?page=register">Register</a>
+            <?php endif; ?>
+        </div>
+    </div>
+
+
+
+
+
+
+    <!-- <div class="navbar">
         <a href="index.php?page=home" style="text-decoration:none">
             <h1 class='font3'>T S<div class="font2">Tech Store</div></h1>
         </a>
@@ -28,7 +64,11 @@ session_start();
                 <a href="index.php?page=register">Register</a>
             <?php endif; ?>
         </div>
-    </div>
+    </div> -->
+
+
+
+
 
     <div class="carousel">
         <div class="carousel-slide">
